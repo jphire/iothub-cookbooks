02b5e4871cb8ab631839a8c39b25e4e03da2139e
jphire <janne.laukkanen83@gmail.com>
2015-10-25 17:54:53 +0200

    install openjdk flavour java

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 
-default['java']['install_flavor'] = 'oracle'
+default['java']['install_flavor'] = 'openjdk'
 default['java']['jdk_version'] = '7'
-default['java']['oracle']['accept_oracle_download_terms'] = true
+
