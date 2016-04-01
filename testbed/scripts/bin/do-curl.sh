26e4bc1449e18742334d32b14df33284e36fe37e
jphire <janne.laukkanen83@gmail.com>
2016-04-01 15:07:38 +0300

    add cpu and memory metering, improve configuration structure

--- testbed/scripts/bin/do-curl.sh
+++ testbed/scripts/bin/do-curl.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-HOST="http://localhost" # e.g. hub1 for VM guests
-FEED="/api/feeds/executable/1/run"
-TYPE="solmuhub"
-KAHVITYPE="kahvihub"
-NODETYPE="node"
-DUKTAPETYPE="duktape"
-JAVATYPE="java"
-SOLMUTYPE="solmuhub"
-KAHVIPORT="8080"
-NODEPORT="3000"
-JAVAPORT="9000"
-DUKTAPEPORT="3030"
-SOLMUPORT="3000"
-LOOPCOUNT="3"
-METHOD="fibonacci" # default to fibonacci script
-SCRIPT="$METHOD.js"
-IOTHUBUSER="anon"
-CONF="solmuhub.conf"
-ACCESS_TOKEN=""
-
 
 function loopX {
 	local total1=0 total2=0 total3=0 total4=0 total5=0 total6=0 total7=0 total8=0 total9=0 total10=0
@@ -31,10 +11,11 @@ function loopX {
     local HOST=$2
     local PORT=$3
     local ADDR=$4
+    local PID=$8
     local LOOPCOUNT=$5
     local SCRIPT="../js/$6"
     local METHOD=$7
-    local ACCESS_TOKEN=$8
+    # local ACCESSTOKEN=$9
     # local stamp=$(date +"%s")
     local filename=$METHOD-$TYPE.out
     local meanfilename=avg-$filename
@@ -49,29 +30,43 @@ function loopX {
     echo "SCRIPT: $SCRIPT"
     echo "LOOPCOUNT: $LOOPCOUNT"
     echo "FULL OUTPUT FILE: $filename"
-    echo "MEAN OUTPUT FILE: ../../results/latest/$meanfilename"
+    echo "MEAN OUTPUT FILE: $AVGPATH/$meanfilename"
 
     # IoT Hub tests
     # Get curl output format from curl-log.txt. Consult that file to find out what is the meaning
     # of each column. The output format should be machine readable, for e.g. gnuplot.
-    CURL_HEADER=""
-    CURL_HEADER="-H 'Content-Type:application/json' "
-    CURL_HEADER="$CURL_HEADER -H 'Accept: application/json' \n"
+    # CURL_HEADER=""
+    # CURL_HEADER="-H 'Content-Type:application/json' "
+    # CURL_HEADER="$CURL_HEADER -H 'Accept: application/json' \n"
 
-    if [ "ACCESS_TOKEN" ]; then
-        CURL_HEADER="$CURL_HEADER -H 'Authorization: $ACCESS_TOKEN' \n"
+    # if [ "ACCESS_TOKEN" ]; then
+    #     CURL_HEADER="$CURL_HEADER -H 'Authorization: $ACCESSTOKEN' \n"
+    # fi
+
+    # measure memory and cpu usage. The files are created first, because there may be a delay
+    # before pidstat creates the files, and we need the files to exist immediately for monitoring
+    # later
+    touch "$STATSPATH/$METHOD-$TYPE-mem.dat"
+    touch "$STATSPATH/$METHOD-$TYPE-cpu.dat"
+    pidstat -r $MEMFRQ $MEMSAMPLE -p $PID >"$STATSPATH/$METHOD-$TYPE-mem.dat" &
+    pidstat $CPUFRQ $CPUSAMPLE -p $PID >"$STATSPATH/$METHOD-$TYPE-cpu.dat" &
+
+    if [ ! -e "$ALLPATH" ] ; then
+        mkdir -p "$ALLPATH" 
     fi
 
     while [ $i -lt $LOOPCOUNT ]; do
         curl -XPOST "$CURL_HEADER" \
             -H 'Content-Type:text/plain' \
             -H 'Accept:text/plain' \
-            -H 'Authorization: $ACCESS_TOKEN' \
+            -H 'Authorization: $ACCESSTOKEN' \
             --data-binary @"$SCRIPT" \
-            -w "@../format/time-total-format.txt" \
-            "$HOST:$PORT$ADDR" -s >>$filename
+            "$HOST:$PORT$ADDR" -s >>$ALLPATH/$filename
+            #-w "@../format/time-total-format.txt" \
         i=$[$i+1]
-        echo "Request $i / $LOOPCOUNT"
+        echo "
+Request $i / $LOOPCOUNT"
+
     done
 
     # Print results to stdout in a convenient way, only mean is showed. To show total result listing, see
@@ -82,22 +77,42 @@ function loopX {
     # echo "| Mean times                                                                                                     |"
     # echo "| ------------------------------------------------------------------------------------------------------------- |"
 
-	# Get mean from each column and print it with awk.
-    if [ ! -e "../../results/latest/$meanfilename" ] ; then
-        touch "../../results/latest/$meanfilename"
+    # Check that stat files are in place
+    while ! grep "Average*" $STATSPATH/$METHOD-$TYPE-mem.dat; do 
+        echo "Waiting for $STATSPATH/$METHOD-$TYPE-mem.dat to appear.."
+        sleep 1; 
+    done
+    
+    while ! grep "Average*" $STATSPATH/$METHOD-$TYPE-cpu.dat; do
+        echo "Waiting for $STATSPATH/$METHOD-$TYPE-cpu.dat to appear.."
+        sleep 1; 
+    done
+
+
+    if [ ! -e "$LATESTPATH/$METHOD" ] ; then
+        mkdir -p "$LATESTPATH/$METHOD" 
     fi
 
-    echo -n > "../../results/latest/$meanfilename"
+    # Note! the pidstat processes need to finish before the Average-row exists in the files
+    cat "$STATSPATH/$METHOD-$TYPE-mem.dat" | grep Average* | sed 's/,/./g' |awk '{ printf "%f", $8 }' >"$LATESTPATH/$METHOD/$TYPE-mem.dat"
+    cat "$STATSPATH/$METHOD-$TYPE-cpu.dat" | grep Average* | sed 's/,/./g' |awk '{ printf "%f", $7 }' >"$LATESTPATH/$METHOD/$TYPE-cpu.dat"
+
+	# Get mean from each column and print it with awk.
+    if [ ! -e "$AVGPATH/$meanfilename" ] ; then
+        touch "$AVGPATH/$meanfilename"
+    fi
+
+    echo -n > "$AVGPATH/$meanfilename"
 
     # awk '{ total1+=$1; total2+=$2; total3+=$3; total4+=$4; total5+=$5; total6+=$6; total7+=$7; total8+=$8; total9+=$9; total10+=$10; reqtime+=$11; c++ } \
     #     END { printf "%f\t%f\t%i\n%f\n%f\n%f\n%f\n%f\n%f\n%f\n%f\n%f\n", total1/c,reqtime/c*1000,c,total2/c,total3/c,total4/c,total5/c,total6/c,total7/c,total8/c,total9/c,total10/c}' "$filename" >>"$meanfilename"
 
-    awk '{ total1+=$1; total2+=$2; total3+=$3; total4+=$4; total5+=$5; total6+=$6; total7+=$7; total8+=$8; total9+=$9; total10+=$10; reqtime+=$11; c++ } \
-        END { printf "%f\n%f\n%f\n%f\n%f\n%f\n%f\n%f\n%f\n%f\n", total1/c,total2/c,total3/c,total4/c,total5/c,total6/c,total7/c,total8/c,total9/c,total10/c}' "$filename" >>"../../results/latest/$meanfilename"
+    awk '{ total1+=$1; total2+=$2; total3+=$3; total4+=$4; total5+=$5; total6+=$6; total7+=$7; total8+=$8; total9+=$9; total10+=$10; c++ } \
+        END { printf "%f\n%f\n%f\n%f\n%f\n%f\n%f\n%f\n%f\n%f\n", total1/c,total2/c,total3/c,total4/c,total5/c,total6/c,total7/c,total8/c,total9/c,total10/c}' "$ALLPATH/$filename" >>"$AVGPATH/$meanfilename"
 
-    rm "$filename"
 	echo "--------------------------------------------------------------------------------------------------------------------------------"
 }
+
 
 while [[ $# > 0 ]]; do
 key="$1"
@@ -111,6 +126,22 @@ case $key in
     LOOPCOUNT="$2"
     shift
     ;;
+    --host)
+    HOST="$2"
+    shift
+    ;;
+    --feed)
+    FEED="$2"
+    shift
+    ;;
+    --type)
+    TYPE="$2"
+    shift
+    ;;
+    --pid) # kahvihub server port
+    PID="$2"
+    shift
+    ;;
     --access-token) # kahvihub server port
     ACCESS_TOKEN="$2"
     shift
@@ -119,61 +150,12 @@ case $key in
     PORT="$2"
     shift
     ;;
-    --kahviport) # kahvihub server port
-    KAHVIPORT="$2"
-    shift
-    ;;
-    --nodeport) # nodejs server port
-    NODEPORT="$2"
-    shift
-    ;;
-    --javaport) # java server port
-    JAVAPORT="$2"
-    shift
-    ;;
-    --duktapeport) # duktape server port
-    DUKTAPEPORT="$2"
-    shift
-    ;;
-    --solmuport) # duktape server port
-    SOLMUPORT="$2"
-    shift
-    ;;
-    --use-kahvihub)
-    PORT="$KAHVIPORT"
-    TYPE="kahvihub"
-    shift
-    ;;
-    --use-node)
-    PORT="$NODEPORT"
-    TYPE="node"
-    shift
-    ;;
-    --use-java)
-    PORT="$JAVAPORT"
-    TYPE="java"
-    shift
-    ;;
-    --use-duktape)
-    PORT="$DUKTAPEPORT"
-    TYPE="duktape"
-    shift
-    ;;
-    --use-solmuhub)
-    PORT="$SOLMUPORT"
-    TYPE="solmuhub"
-    shift
-    ;;
     --script)
     SCRIPT="$2"
     shift
     ;;
     --method)
     METHOD="$2"
-    shift
-    ;;
-    --conf)
-    CONF="$2"
     shift
     ;;
     --default)
@@ -186,23 +168,17 @@ esac
 shift # past argument or value
 done
 
-if [ -r "../conf.d/$CONF" ]; then
-  echo "Reading user config for iothub from file $CONF...." >&2
-  . "../conf.d/$CONF"
+echo "$TYPE $HOST $PORT $FEED $LOOPCOUNT $SCRIPT $METHOD $PID $ACCESSTOKEN "
+loopX $TYPE $HOST $PORT $FEED $LOOPCOUNT $SCRIPT $METHOD $PID
+
+
+if [ ! -e "$LATESTPATH/$METHOD/$TYPE.dat" ] ; then
+    touch "$LATESTPATH/$METHOD/$TYPE.dat"
 fi
-
-echo "$TYPE $HOST $PORT $FEED $LOOPCOUNT $SCRIPT $METHOD $ACCESS_TOKEN"
-loopX $TYPE $HOST $PORT $FEED $LOOPCOUNT $SCRIPT $METHOD $ACCESS_TOKEN
+echo -n >"$LATESTPATH/$METHOD/$TYPE.dat"
 
 
-# 
-if [ ! -e "../../results/latest/$METHOD/$TYPE.dat" ] ; then
-    mkdir -p "../../results/latest/$METHOD" 
-    touch "../../results/latest/$METHOD/$TYPE.dat"
-fi
-echo -n >"../../results/latest/$METHOD/$TYPE.dat"
-
-
-paste "../templates/$METHOD-numbers" "../../results/latest/avg-$METHOD-$TYPE.out"  >>"../../results/latest/$METHOD/$TYPE.dat"
+paste "../templates/$METHOD-numbers" "$AVGPATH/avg-$METHOD-$TYPE.out" "$LATESTPATH/$METHOD/$TYPE-cpu.dat" \
+"$LATESTPATH/$METHOD/$TYPE-mem.dat" >>"$LATESTPATH/$METHOD/$TYPE.dat"
 
 
--- testbed/scripts/bin/iothub-make.sh
+++ testbed/scripts/bin/iothub-make.sh
@@ -5,9 +5,21 @@
 # indicates when they have been moved there, not the date that the tests in those folders where run. Possibly the next former
 # timestamped folder in results is the timestamp of the next newer tests.. pretty complicated but.. yeah. 
 
-types=( "kahvihub" "node" "duktape" "solmuhub")
-methods=( "fibonacci" "quicksort" "newton")
+types=( "solmuhub" "kahvihub" "node" "duktape" )
+methods=( "newton" "quicksort" "newton" )
 times=1
+scr="Scripts"
+CONF="main.conf"
+
+. "config.sh"
+
+if [ -r "../conf.d/$CONF" ]; then
+  echo "Reading user config for iothub from $CONF...." >&2
+  . "../conf.d/$CONF"
+else
+    echo "$CONF file not found in conf.d"
+    exit 1
+fi
 
 time_stamp=$(date +"%s")
 pretty_date=$(date -d @$time_stamp)
@@ -15,41 +27,52 @@ BACKUPDIR=$time_stamp
 TEMPLATE_DIR=templates
 BIN_DIR=bin
 
-mkdir -p ../../results/$BACKUPDIR
-mv ../../results/latest/* ../../results/$BACKUPDIR
+#mkdir -p $RESULTSPATH/$BACKUPDIR
+mv $LATESTPATH $RESULTSPATH/$BACKUPDIR
+#rm -r $LATESTPATH/*
+mkdir -p $LATESTPATH
 
-touch "../../results/latest/TEST-SPEC"
-printf "Test was generated at: %s\nLoop times: %d\n" "$pretty_date" "$times" >>../../results/latest/TEST-SPEC
-
-for m in "${methods[@]}";
-do
-    for t in "${types[@]}";
-    do
-        if [ "$t" == "duktape" ];
-        then
-            SCRIPT="$m-nocall.js"
-        else
-            SCRIPT="$m-sm.js"
-        fi
-
-        # echo "./do-curl.sh --method $m --script $SCRIPT --conf $t.conf"
-        ./do-curl.sh -n $times --method $m --script $SCRIPT --conf $t.conf
-    done
-    # Uncomment to save intermediate results to files, for debugging
-    # awk '{ total1+=$1; total2+=$2; total3+=$3; total4+=$4; total5+=$5; total6+=$6; total7+=$7; total8+=$8; total9+=$9; total10+=$10; reqtime+=$11; c++ } \
-    #     END { printf "%f\t%f\t%i\n%f\n%f\n%f\n%f\n%f\n%f\n%f\n%f\n%f\n", total1/c,reqtime/c*1000,c,total2/c,total3/c,total4/c,total5/c,total6/c,total7/c,total8/c,total9/c,total10/c}' "$filename" >>"$meanfilename"
-done
-
-mkdir -p ../../results/latest/plot
+touch $LATESTPATH/TEST-SPEC
+mkdir -p $STATSPATH
+mkdir -p $AVGPATH
+printf "Test was started at: %s\nLoop times: %d\n" "$pretty_date" "$times" >>"$LATESTPATH/TEST-SPEC"
 
 for method in "${methods[@]}";
 do
-    if [ ! -e "../../results/latest/$method.dat" ] ; then 
-        touch "../../results/latest/$method.dat"
+    for type in "${types[@]}";
+    do
+        tmp=$type$scr[$method]
+        tmp_script="${!tmp}"
+        tmpPid="${type}Pid"
+        pid="${!tmpPid}"
+        feedName="${type^^}FEED"
+        feed="${!feedName}"
+        portName="${type^^}PORT"
+        port="${!portName}"
+
+        echo "./do-curl.sh -n $times --method $method --script $tmp_script --pid $pid --port $port --feed $feed --type $type --host $HOST"
+        . do-curl.sh -n $times --method $method --script $tmp_script --pid $pid --port $port --feed $feed --type $type --host $HOST
+    done
+    # Uncomment to save intermediate results to files, for debugging
+    # results_file="../../results/latest/all-$m-$t.out"
+    # awk '{ total1+=$1; total2+=$2; total3+=$3; total4+=$4; total5+=$5; total6+=$6; total7+=$7; total8+=$8; total9+=$9; total10+=$10; reqtime+=$11; c++ } \
+    #     END { printf "%f\t%f\t%i\n%f\n%f\n%f\n%f\n%f\n%f\n%f\n%f\n%f\n", total1/c,reqtime/c*1000,c,total2/c,total3/c,total4/c,total5/c,total6/c,total7/c,total8/c,total9/c,total10/c}' \
+    #     "$filename" >>"$results_file"
+done
+
+mkdir -p $LATESTPATH/plot
+
+for method in "${methods[@]}";
+do
+    if [ ! -e "$LATESTPATH/$method.dat" ] ; then 
+        touch "$LATESTPATH/$method.dat"
     fi
-    paste ../$TEMPLATE_DIR/$method-numbers ../../results/latest/avg-$method-kahvihub.out ../../results/latest/avg-$method-node.out \
-    ../../results/latest/avg-$method-duktape.out ../../results/latest/avg-$method-solmuhub.out >../../results/latest/$method.dat
+
+    paste ../$TEMPLATE_DIR/$method-numbers $AVGPATH/avg*  >"$LATESTPATH/$method.dat"
 
     gnuplot ../plot/plot-$method.p
 done
 
+end_stamp=$(date +"%s")
+end_pretty_date=$(date -d @$end_stamp)
+printf "\nTest ended at: %s\nLoop times: %d\n" "$end_pretty_date" "$times" >>"$LATESTPATH/TEST-SPEC"
